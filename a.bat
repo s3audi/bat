c4653bee9c6a6b7ba6e7b500d5578702950eaf00
s3audi <s3audi@hotmail.com>
2023-10-12 08:30:54 +0300

    Update a.bat 2

--- a.bat
+++ a.bat
@@ -1,4 +1,4 @@
-start opera.exe https://miro.com/app/board/uXjVNbwdwuw=/?share_link_id=302203305388 https://coda.io/d/Harcamalaaa_d834zlyoOUo/Ekim-2023_su9xp#_lu3KM https://chat.openai.com/c/b8207024-8b6d-4a9d-8a39-7940cb1b0b7f
+start opera.exe https://miro.com/app/board/uXjVNbwdwuw=/?share_link_id=302203305388 https://coda.io/d/Harcamalaaa_d834zlyoOUo/Ekim-2023_su9xp#_lu3KM https://chat.openai.com/c/b8207024-8b6d-4a9d-8a39-7940cb1b0b7f https://github.com/s3audi/bat/edit/main/a.bat
 
 Miro 
 Coda Ekim
